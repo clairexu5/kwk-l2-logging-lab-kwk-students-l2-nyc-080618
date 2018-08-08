0db189bb2f9bf800abde92618d6f8d9802b3b2f2
Claire Xu <claire02.xu@gmail.com>
2018-08-08 17:18:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const driver = 'bob';
 console.log("this code was called");
 console.log(driver);
-console.log("this code was called", "is the " + driver + );
+console.log("this code was called", "is the " + driver);
